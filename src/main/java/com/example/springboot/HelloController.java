5e6895b95b77676be5d38018f7817b9017f285df
letthefireflieslive <ln@outlook.ph>
2022-05-05 14:19:43 +0800

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	@GetMapping("/hello")
+	public String greet() {
+		String visitor = System.getenv().getOrDefault("VISITOR", "Anonymous");
+		return "Hello " + visitor;
+	}
+	
 	@GetMapping("/")
 	public String index() {
 		return "Welcome to App 2";
